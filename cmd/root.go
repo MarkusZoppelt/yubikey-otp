80776fa3adbb8f28f94b3007e9b6aa1271f427f6
Markus Zoppelt <markus@zoppelt.net>
2023-01-12 12:36:47 +0100

    fix: more helpful error messages

--- cmd/root.go
+++ cmd/root.go
@@ -6,6 +6,7 @@ import (
 	"os/exec"
 
 	"github.com/ktr0731/go-fuzzyfinder"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
 	"github.com/MarkusZoppelt/yubikey-otp/internal/accounts"
@@ -22,7 +23,7 @@ searchable list. Select the code you want to copy to the clipboard.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		accounts, err := accounts.GetOTPAccountsFromYubiKey()
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("Please make sure that the YubiKey is connected and that ykman is installed.")
 			os.Exit(1)
 		}
 
@@ -44,7 +45,7 @@ searchable list. Select the code you want to copy to the clipboard.`,
 		fmt.Printf("Please touch your YubiKey to generate the OTP code for %s\n", accounts[idx])
 		err = exec.Command("sh", "-c", cmdStr).Run()
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("Error copying to clipboard: ", errors.WithStack(err))
 			os.Exit(1)
 		}
 		fmt.Printf("Copied OTP code for %s to clipboard.\n", accounts[idx])
