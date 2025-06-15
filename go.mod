3fcdad02574bc06abc82c3a4c2b2744ba46d8720
Markus Zoppelt <markus@zoppelt.net>
2025-06-15 22:47:21 +0200

    feat: pure go oath

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/MarkusZoppelt/yubikey-otp
 go 1.24
 
 require (
+	cunicu.li/go-iso7816 v0.8.6
+	cunicu.li/go-ykoath/v2 v2.1.22
 	github.com/atotto/clipboard v0.1.4
+	github.com/ebfe/scard v0.0.0-20241214075232-7af069cabc25
 	github.com/ktr0731/go-fuzzyfinder v0.9.0
 	github.com/pterm/pterm v0.12.66
 	github.com/spf13/cobra v1.9.1
@@ -38,9 +41,10 @@ require (
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/crypto v0.39.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/sys v0.32.0 // indirect
-	golang.org/x/term v0.31.0 // indirect
-	golang.org/x/text v0.24.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/term v0.32.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/devices/devices.go
+++ internal/devices/devices.go
@@ -1,20 +1,45 @@
 package devices
 
 import (
-	"bufio"
-	"os/exec"
-	"strings"
+	"fmt"
+
+	"cunicu.li/go-iso7816/drivers/pcsc"
+	"cunicu.li/go-ykoath/v2"
+	"github.com/ebfe/scard"
 )
 
 func GetDevices() (devices []string, err error) {
-	out, err := exec.Command("ykman", "list").Output()
+	ctx, err := scard.EstablishContext()
+	if err != nil {
+		return nil, err
+	}
+	defer ctx.Release()
+
+	readers, err := ctx.ListReaders()
 	if err != nil {
 		return nil, err
 	}
 
-	scanner := bufio.NewScanner(strings.NewReader(string(out)))
-	for scanner.Scan() {
-		devices = append(devices, scanner.Text())
+	for _, reader := range readers {
+		card, err := pcsc.NewCard(ctx, reader, true)
+		if err != nil {
+			continue
+		}
+
+		ykoathCard, err := ykoath.NewCard(card)
+		if err != nil {
+			card.Close()
+			continue
+		}
+
+		_, err = ykoathCard.Select()
+		if err != nil {
+			ykoathCard.Close()
+			continue
+		}
+
+		devices = append(devices, fmt.Sprintf("%s", reader))
+		ykoathCard.Close()
 	}
 
 	return devices, nil
